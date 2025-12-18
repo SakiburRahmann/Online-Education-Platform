5b1298ff46186d47bea66eb0ce349ce99e51ca39
Sakibur Rahman <sakiburrahmannnn@gmail.com>
2025-12-19 03:18:48 +0600

    Allow seeding in production environment

--- backend/seed_users.py
+++ backend/seed_users.py
@@ -2,7 +2,7 @@ import os
 import django
 from django.contrib.auth import get_user_model
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ.get('DJANGO_SETTINGS_MODULE', 'config.settings.development'))
 django.setup()
 
 User = get_user_model()
