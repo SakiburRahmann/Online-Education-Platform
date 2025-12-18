d866af9b8513dd7a9e25d20a50992d6eed8d4722
Sakibur Rahman <sakiburrahmannnn@gmail.com>
2025-12-19 03:09:28 +0600

    Automate production migrations and optimize startup

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -o errexit
 # Install dependencies from root requirements.txt
 pip install -r requirements.txt
 
+# Run migrations during build
+python backend/manage.py migrate --no-input
+
 # Collect static files
 python backend/manage.py collectstatic --no-input
 
